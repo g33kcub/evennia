2da9c8620b26085e748389bcece24152913e8277
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-15 11:19:48 -0500

    add new typeclass
    Added 2 new Type Classes:
     - NPC - For our NPCs.
     - Monster - For our monsters.

---
+++ typeclasses/monsters.py
@@ -0,0 +1,12 @@
+"""
+Monster
+
+The monster is the default "Naked" object for all of monsters
+in the game world.
+
+"""
+
+from evennia import DefaultObject
+
+class Monster(DefaultObject):
+pass 
---
+++ typeclasses/npcs.py
@@ -0,0 +1,12 @@
+"""
+NPC
+
+The npc is the default "Naked" object for all of npcss
+in the game world.
+
+"""
+
+from evennia import DefaultObject
+
+class NPC(DefaultObject):
+pass 
