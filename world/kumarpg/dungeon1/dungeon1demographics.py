5fa174856feafe807bd89f62c2861b693ae561b7
g33kcub <g33kcub@gmail.com>
2019-12-16 11:24:09 -0500

    Update dungeon1demographics.py

--- world/kumarpg/dungeon1/dungeon1demographics.py
+++ world/kumarpg/dungeon1/dungeon1demographics.py
@@ -4,16 +4,16 @@ This is the demographics file for Dungeon 1.
 """
 
 def dungeon1_demographics(character):
-    character.db.d1.rpgclass = "Citizen"
-    character.db.d1.title = ""
-    character.db.d1.rpglevel = 1
+    character.db.d1_rpgclass = "Citizen"
+    character.db.d1_title = ""
+    character.db.d1_rpglevel = 1
     """
     Combat System Stuff
     """
-    character.db.d1.pool_mana = {"max": 0, "cur": 0}
-    character.db.d1.pool_health = {"max": 0, "cur": 0}
-    character.db.d1.pool_fatigue = {"max": 0, "cur": 0}
-    character.db.d1.pool_ego = {"max": 0, "cur": 0}
-    character.db.d1.conditions = {}
+    character.db.d1_pool_mana = {"max": 0, "cur": 0}
+    character.db.d1_pool_health = {"max": 0, "cur": 0}
+    character.db.d1_pool_fatigue = {"max": 0, "cur": 0}
+    character.db.d1_pool_ego = {"max": 0, "cur": 0}
+    character.db.d1_conditions = {}
 
  
