6a2efe3c26eba5b137926e98c34dddea8a90a0d6
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-15 22:14:15 -0500

    Shifting to World
    Moved all the character stats to world.kumarpg.statdb > character_stats()

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -9,6 +9,7 @@ creation commands.
 """
 from evennia import DefaultCharacter
 from typeclasses.npcs import NPC
+from world.kumarpg.statdb import character_stats
 
 
 class Character(DefaultCharacter):
@@ -31,91 +32,6 @@ class Character(DefaultCharacter):
     at_post_puppet - Echoes "AccountName has entered the game" to the room.
 
     """
-    """
-    Skill Types
-    0 = General
-    1 = Weapon
-    2 = Social
-    3 = Crafting
-    4 = Tradesman
-
-    Crafting Skill Types:
-    0 = None
-    1 = Smithing
-    2 = Tailoring
-    3 = Production
-    4 = Scholary
-    5 = Chef
-    """
     def at_object_creation(self):
-        stat_dict = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0 }
-        stat_dict_magic = {"rank": 0, "lxp": 0, "mp": 0, "lxpl": 0, "cor": 0, "hl": 0}
-        skill_dict = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 0 }
-        skill_dict_weapon = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 1 }
-        skill_dict_social = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 2 }
-        skill_dict_crafting = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 0 }
-        skill_dict_crafting1 = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 1 }
-        skill_dict_crafting2 = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 2 }
-        skill_dict_crafting3 = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 3 }
-        skill_dict_crafting4 = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 4 }
-        skill_dict_crafting5 = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 3, "group": 5 }
-        skill_dict_tradesman = { "rank" : 0, "lxp" : 0, "mp" : 0, "lxpl" : 0, "cor" : 0, "hl": False, "type": 4 }
-        self.db.stats = {
-            "earth": stat_dict,
-            "fire": stat_dict,
-            "air": stat_dict,
-            "water": stat_dict,
-            "void": stat_dict
-        }
+        character_stats(self)
 
-        self.db.skills = {
-            "swords": skill_dict_weapon,
-            "knives": skill_dict_weapon,
-            "hammers" : skill_dict_weapon,
-            "whips": skill_dict,
-            "spears": skill_dict_weapon,
-            "staves": skill_dict_weapon,
-            "axes": skill_dict_weapon,
-            "bows": skill_dict_weapon,
-            "fans": skill_dict_weapon,
-            "rods": skill_dict_weapon,
-            "wands": skill_dict_weapon,
-            "persuasion": skill_dict_social,
-            "haggling": skill_dict_social,
-            "intimidation": skill_dict_social,
-            "empathy": skill_dict_social,
-            "subterfuge":skill_dict_social,
-            "leadership": skill_dict_social,
-            "command": skill_dict_social,
-            "socialize": skill_dict_social,
-            "etiquette": skill_dict_social,
-            "armor smith":skill_dict_crafting1,
-            "weapon smith":skill_dict_crafting1,
-            "black smith":skill_dict_crafting1,
-            "jeweler":skill_dict_crafting1,
-            "carpenter":skill_dict_crafting1,
-            "leather working":skill_dict_crafting2,
-            "seamstress":skill_dict_crafting2,
-            "miner":skill_dict_crafting3,
-            "forager":skill_dict_crafting3,
-            "farmer":skill_dict_crafting3,
-            "fisher":skill_dict_crafting3,
-            "scavenger":skill_dict_crafting3,
-            "animal husbandry":skill_dict_crafting3,
-            "enchanter":skill_dict_crafting4,
-            "scribe":skill_dict_crafting4,
-            "alchemy":skill_dict_crafting5,
-            "culinarian":skill_dict_crafting5,
-            "appraisal":skill_dict_tradesman,
-            "research":skill_dict_tradesman
-        }
-
-        self.db.magic = {
-            "pyro": stat_dict_magic,
-            "aqua": stat_dict_magic,
-            "aero": stat_dict_magic,
-            "terra": stat_dict_magic,
-            "cure": stat_dict_magic,
-            "shield": stat_dict_magic,
-            "esuna": stat_dict_magic
-        }
