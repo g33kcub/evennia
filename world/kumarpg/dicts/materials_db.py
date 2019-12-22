3eab2aa9160268230b1068e2a6d4eb983bfa72cb
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-21 21:26:46 -0500

    Create materials_db.py

---
+++ world/kumarpg/dicts/materials_db.py
@@ -0,0 +1,56 @@
+"""
+This is a database of all the materials in the system. 
+More will get added as new dungeons are introduced. 
+Materials are limited to a specific dungeon, will be statted as such.
+
+Material Formatting:
+
+NAME      = STR = A bit obvious.
+Tier      = INT = This determines the generic value of it when combined with rarity.
+Rarity    = STR = How often it is found during the month cycle. 
+Table     = []  = Global, D1, D2, etc Can be more than one. 
+TYPE      = STR = What type of material is. Plant, etc. 
+Desc      = STR = Description of it. 
+QUIRK     = STR = A list of any magical or unique properties. 
+"""
+
+MATERIAL_TABLE = {
+    "pelt, low quality": {
+        "tier": 0,
+        "rarity": "common",
+        "table": ['Global'],
+        "type": "beast part",
+        "desc": "A pelt of any animal, though badly cut from them.",
+        "quirk": "none"
+    },
+    "pelt": {
+        "tier": 1,
+        "rarity": "common",
+        "table": ['Global'],
+        "type": "beast part",
+        "desc": "A pelt of any animal, skinned by someone with some basic skill.",
+        "quirk": "none"
+    },
+    "pelt, high quality": {
+        "tier": 2,
+        "rarity": "uncommon",
+        "table": ['Global'],
+        "type": "beast part",
+        "desc": "A pelt of any animal, skinned by someone with masterful skills.",
+        "quirk": "Adds +1 to armor made with it."
+    },
+    "pelt, exotic": {
+        "tier": 3,
+        "rarity": "rare",
+        "table": ['Global'],
+        "desc": "A pelt of a rare and exotic creature, maybe even mythical, skinned with great care.",
+        "quirk": "Adds +3 to armor made with it."
+    },
+    "ice wolf pelt": {
+        "tier": 5,
+        "rarity": "rare",
+        "table": ['D1'],
+        "desc": "A blue and white furred pelt that is ice cold to the touch, it was skinned with great care.",
+        "quirk": "Armor made with this pelt gains a high resistance to ice damage."
+    }
+}
