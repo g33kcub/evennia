69fcfca0f1bb7a2ca8b26ba02a7d9b227a25b18d
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-29 10:25:20 -0500

    adding techniques

---
+++ world/kumarpg/dicts/d1_skills_defs.py
@@ -0,0 +1,101 @@
+from world.kumarpg.dicts.types_defs import types
+
+skills = {
+    "skill": types["skill_dict"],
+    "persuasion": types["skill_dict_social"],
+    "haggling": types["skill_dict_social"],
+    "intimidation": types["skill_dict_social"],
+    "empathy": types["skill_dict_social"],
+    "subterfuge": types["skill_dict_social"],
+    "leadership": types["skill_dict_social"],
+    "command": types["skill_dict_social"],
+    "socialize": types["skill_dict_social"],
+    "etiquette": types["skill_dict_social"],
+    "armor smith": types["skill_dict_crafting1"],
+    "weapon smith": types["skill_dict_crafting1"],
+    "black smith": types["skill_dict_crafting1"],
+    "jeweler": types["skill_dict_crafting1"],
+    "tinkerer": types["skill_dict_crafting1"],
+    "carpenter": types["skill_dict_crafting1"],
+    "leather working": types["skill_dict_crafting2"],
+    "seamstress": types["skill_dict_crafting2"],
+    "miner": types["skill_dict_crafting3"],
+    "forager": types["skill_dict_crafting3"],
+    "farmer": types["skill_dict_crafting3"],
+    "fisher": types["skill_dict_crafting3"],
+    "scavenger": types["skill_dict_crafting3"],
+    "animal husbandry": types["skill_dict_crafting3"],
+    "enchanter": types["skill_dict_crafting4"],
+    "scribe": types["skill_dict_crafting4"],
+    "alchemy": types["skill_dict_crafting5"],
+    "culinarian": types["skill_dict_crafting5"],
+    "appraisal": types["skill_dict_tradesman"],
+    "research": types["skill_dict_tradesman"],
+    "survival": types["skill_dict_tradesman"]
+}
+
+persuasion_techniques = {
+    "smooth talking": types["tech_dict"],
+    "fast talking": types["tech_dict"]
+}
+
+leadership_techniques = {
+    "inspire": types["tech_dict"]
+}
+
+animal_husbandry_techniques = {
+    "good breeding": types["tech_dict"],
+    "quick training": types["tech_dict"]
+}
+
+survival_techniques = {
+    "track": types["tech_dict"]
+}
+
+
+scribe_techniques = {
+    "quick writing": types["tech_dict"],
+    "copy writing": types["tech_dict"]
+}
+
+research_techniques = {
+    "hidden clue": types["tech_dict"]
+}
+
+alchemy_techniques = {
+    "swift brewing": types["tech_dict"],
+    "deep draught": types["tech_dict"]
+}
+
+socialize_techniques = {
+    "sharp tongue": types["tech_dict"],
+    "witty banter": types["tech_dict"]
+}
+
+culinarian_techniques = {
+    "soul food": types["tech_dict"],
+    "lasting meal": types["tech_dict"]
+}
+
+seamstress_techniques = {
+    "reinforced stitch": types["tech_dict"],
+    "swift stitching": types["tech_dict"],
+}
+
+MAGIC_SKILLS = {
+    "fire magic": types["magic_dict"],
+    "water magic": types["magic_dict"],
+    "air magic": types["magic_dict"],
+    "earth magic": types["magic_dict"],
+    "shadow magic": types["magic_dict"],
+    "holy magic": types["magic_dict"],
+    "lightning magic": types["magic_dict"],
+    "ice magic": types["magic_dict"],
+    "mana magic": types["magic_dict"],
+    "abjuration": types["magic_dict"],
+    "healing magic": types["magic_dict"],
+    "time magic": types["magic_dict"],
+    "chaos magic": types["magic_dict"],
+    "status magic": types["magic_dict"],
+    "summoning": types["magic_dict"]
+}
--- world/kumarpg/dicts/d1_weapon_defs.py
+++ world/kumarpg/dicts/d1_weapon_defs.py
@@ -12,5 +12,86 @@ weapons = {
     "fans": types["skill_dict_weapon"],
     "rods": types["skill_dict_weapon"],
     "wands": types["skill_dict_weapon"],
-    "battle shields": types["skill_dict_weapon"]
+    "battle shields": types["skill_dict_weapon"],
+    "martial arts": types["skill_dict_weapon"]
 }
+
+martial_arts_techniques = {
+    "quick strike": types["tech_dict"],
+    "heavy strike": types["tech_dict"],
+    "knockback strike": types["tech_dict"],
+    "uppercut": types["tech_dict"],
+    "roundhouse kick": types["tech_dict"]
+}
+
+battle_shields_techniques = {
+    "hold the line": types["tech_dict"],
+    "shield bash": types["tech_dict"],
+}
+
+wands_techniques = {
+    "quick shot": types["tech_dict"],
+    "counter shot": types["tech_dict"],
+    "power shot": types["tech_dict"]
+}
+
+rods_techniques = {
+    "parry": types["tech_dict"],
+    "heavy swing": types["tech_dict"],
+    "roundhouse strike": types["tech_dict"]
+}
+
+fans_techniques = {
+    "parry": types["tech_dict"],
+    "swift strike": types["tech_dict"],
+    "throat strike": types["tech_dict"]
+}
+
+bows_techniques = {
+    "head shot": types["tech_dict"],
+    "perfected aim": types["tech_dict"],
+    "quick shot": types["tech_dict"],
+    "extended shot": types["tech_dict"]
+}
+
+axes_techniques = {
+    "heavy swing": types["tech_dict"],
+    "bladed uppercut": types["tech_dict"],
+    "skull cracker": types["tech_dict"]
+}
+
+staves_techniques = {
+    "parry": types["tech_dict"],
+    "heavy swing": types["tech_dict"],
+    "leg strike": types["tech_dict"]
+}
+
+spears_techniques = {
+    "power thrust": types["tech_dict"],
+    "parry": types["tech_dict"],
+    "spinning strike": types["tech_dict"]
+}
+
+whips_techniques = {
+    "double strike": types["tech_dict"],
+}
+
+swords_techniques = {
+    "heavy swing": types["tech_dict"],
+    "swift strike": types["tech_dict"],
+    "parry": types["tech_dict"],
+    "sword defense": types["tech_dict"]
+}
+
+knives_techniques = {
+    "punch strike": types["tech_dict"],
+    "swift strike": types["tech_dict"],
+    "parry": types["tech_dict"]
+}
+
+hammers_techniques = {
+    "heavy swing": types["tech_dict"],
+    "crushing blow": types["tech_dict"],
+    "spinning volley": types["tech_dict"]
+}
+
