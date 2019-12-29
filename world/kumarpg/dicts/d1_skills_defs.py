7a74943a3c59c67dd11678c084d24192bb1e7c1c
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-29 11:10:29 -0500

    redefining spells as techniques

--- world/kumarpg/dicts/d1_skills_defs.py
+++ world/kumarpg/dicts/d1_skills_defs.py
@@ -97,5 +97,129 @@ MAGIC_SKILLS = {
     "time magic": types["magic_dict"],
     "chaos magic": types["magic_dict"],
     "status magic": types["magic_dict"],
-    "summoning": types["magic_dict"]
+    "summoning": types["magic_dict"],
+    "poison magic": types["magic_dict"]
+}
+
+poison_magic_techniques = {
+    "bio": types["tech_dict"], # Poison Status
+    "acid": types["tech_dict"] # Burn Status
+}
+
+summoning_techniques = {
+    "undine": types["tech_dict"], # Water
+    "wisp": types["tech_dict"], # Air
+    "gnome": types["tech_dict"], # Earth
+    "shade": types["tech_dict"], # Shadow
+    "luna": types["tech_dict"], # light/Holy
+    "flashbug": types["tech_dict"], # Lightning
+    "shiva": types["tech_dict"], # Ice
+    "magus": types["tech_dict"], # Mana
+    "salamander": types["tech_dict"], # Fire
+    "jinx": types["tech_dict"], # Chaos
+    "toxica": types["tech_dict"] # Poison
+}
+
+lightning_magic_techniques = {
+    "shockbolt": types["tech_dict"],
+    "static weapon": types["tech_dict"],
+    "air lense": types["tech_dict"],
+    "raging thunderstorm": types["tech_dict"],
+    "crimson lightning": types["tech_dict"],
+    "plasma strike": types["tech_dict"]
+}
+
+status_magic_techniques = {
+    "medusa's gaze": types["tech_dict"], # Paralyze
+    "earshock": types["tech_dict"], # Deafen
+    "blinding flash": types["tech_dict"], # Blind
+    "psion wave": types["tech_dict"], # Confuse
+    "white noise": types["tech_dict"], # Mute/Silence
+    "attack up": types["tech_dict"],
+    "defense up": types["tech_dict"],
+    "armor break": types["tech_dict"],
+    "power break": types["tech_dict"]
+}
+
+chaos_magic_techniques = {
+    "chaosbolt": types["tech_dict"],
+    "entropy field": types["tech_dict"],
+    "wild weapon": types["tech_dict"],
+    "chaotic bombardment": types["tech_dict"]
+}
+
+time_magic_techniques = {
+    "haste": types["tech_dict"],
+    "slow": types["tech_dict"],
+    "stop": types["tech_dict"],
+    "gravity crush": types["tech_dict"]
+}
+
+healing_magic_techniques = {
+    "healing energy": types["tech_dict"],
+    "liminal energy": types["tech_dict"],
+    "regeneration": types["tech_dict"]
+}
+
+abjuration_techniques = {
+    "protection": types["tech_dict"],
+    "reflection": types["tech_dict"]
+}
+
+mana_magic_techniques = {
+    "manabolt": types["tech_dict"],
+    "counterspell": types["tech_dict"],
+    "arcane strike": types["tech_dict"],
+    "mana weapon": types["tech_dict"]
+}
+
+ice_magic_techniques = {
+    "arcticbolt": types["tech_dict"],
+    "snowstorm": types["tech_dict"],
+    "blizzard weapon": types["tech_dict"],
+    "arctic snap": types["tech_dict"]: 
+}
+
+holy_magic_techniques = {
+    "lightbolt": types["tech_dict"],
+    "gleaming weapon": types["tech_dict"],
+    "gleaming aura": types["tech_dict"],
+    "divine judgment": types["tech_dict"],
+    "sanctuary": types["tech_dict"],
+    "rebuke": types["tech_dict"]
+}
+
+shadow_magic_techniques = {
+    "voidbolt": types["tech_dict"],
+    "blackhole horizon": types["tech_dict"],
+    "shadow weapon": types["tech_dict"],
+    "blackhole supernova": types["tech_dict"]
+}
+
+earth_magic_techniques = {
+    "geobolt": types["tech_dict"],
+    "seismic tremor": types["tech_dict"],
+    "sandstorm weapon": types["tech_dict"],
+    "earthquake": types["tech_dict"]
+}
+
+air_magic_techniques = {
+    "aerobolt": types["tech_dict"],
+    "tornado weapon": types["tech_dict"],
+    "windscreen": types["tech_dict"],
+    "heaven's shockwave": types["tech_dict"]
+}
+
+water_magic_techniques = {
+    "aquabolt": types["tech_dict"],
+    "bubble screen": types["tech_dict"],
+    "whirlpool weapon": types["tech_dict"],
+    "tidal shockwave": types["tech_dict"]
+}
+
+fire_magic_techniques = {
+    "firebolt": types["tech_dict"],
+    "will-o-wisp": types["tech_dict"],
+    "phoenix weapon": types["tech_dict"],
+    "phoenix flare": types["tech_dict"]
 }
--- world/kumarpg/dicts/spell_defs.py
+++ world/kumarpg/dicts/spell_defs.py
@@ -29,23 +29,12 @@ Required Values for a spell:
    * status_duration (int): How long does the status last. 
    * max_targets (int): How many targets can the spell affect at the most.
    * attack_count (int): How many simulatenous attacks does the spell have? (1 default)
-   * object_key (str): The object the spell summons. 
    * elemental (str): The element the spell is aligned with. (non-elemental, default)
-   * dice_level (array): The dice used by default. (['4','4','6','6','8','8','10','12','20'])
-   * max_hp (int): This is the base maximum damage it will take before failing.  (999 is absolute max.)
-   * max_rank (int): This is the maximum rank this spell can be, default is 10(X)
+
 ----------------
  The following are required. They determine values.
 ----------------
    * Accuracy (int): How accurate is the spell in percents? (100 is autohit)
-   * Min_DMG (int): This is the minimum damage the spell does. If the damage roll rolls less than, then this is applied.
-                    If set to 0, this is a non-damaging spell. 
-   * desc_start (str): This is what the description of the spell if it starts the sentence. (A jet of flame)
-   * desc_inc (str): This is what the description of the spell if it is in the sentence. (jets of flame)
-   * cast_action (str): This is what happens when you cast the spell. (chants default)
-      * chant - chants 
-      * sign - signs
-      * scribe - enscribes
 
 ----------------
  System Documentation
@@ -62,148 +51,94 @@ Required Values for a spell:
 """
 
 SPELLS = {
-    "aquabolt": {},
-    "bubble screen": {},
-    "whirlpool weapon": {},
-    "tidal shockwave": {},
+    # poison spells.
+    "bio": {},
+    "acid": {},
+    # summoning spells.
     "undine": {},
-    "aerobolt": {},
-    "windscreen": {},
-    "tornado weapon": {},
-    "heaven's shockwave": {},
     "wisp": {},
-    "geobolt": {},
-    "seismic tremor": {},
-    "sandstorm weapon": {},
-    "earthquake": {},
     "gnome": {},
-    "voidbolt": {},
-    "blackhole horizon": {},
-    "shadow weapon": {},
-    "blackhole supernova": {},
     "shade": {},
-    "lightbolt": {},
-    "gleaming aura": {},
-    "gleaming weapon": {},
-    "divine judgment": {},
     "luna": {},
+    "flashbug": {},
+    "shiva": {},
+    "magus": {},
+    "salamander": {},
+    "jinx": {},
+    "toxica": {},
+    # lightning spells.
     "shockbolt": {},
     "static weapon": {},
     "air lense": {},
-    "raging thunderstorm": {},
-    "flashbug": {},
-    "arcticbolt": {},
-    "snowstorm": {},
-    "blizzard weapon": {},
-    "arctic snap": {},
-    "shiva": {},
-    "manabolt": {},
-    "counterspell": {},
-    "mana weapon": {},
-    "arcane strike": {},
-    "magus": {},
-    "protection": {},
-    "reflection": {},
-    "liminal light": {},
-    "healing energy": {},
-    "haste": {},
-    "slow": {},
-    "sanctuary": {},
-    "gravity crush": {},
     "crimson lightning": {},
+    "plasma strike": {},
+    # status spells
     "medusa's gaze": {},
-    "blinding light": {},
     "earshock": {},
+    "blinding flash": {},
+    "white noise": {},
     "psion wave": {},
-    "rebuke": {},
     "attack up": {},
     "defense up": {},
     "armor break": {},
     "power break": {},
-    "hex": {},
-    "chaosbolt": {
-        "spellfunction": spell_attack_wild,
-        "cost": 2,
-        "target": "other",
-        "elemental": "chaos",
-        "accuracy": 85,
-        "min_dmg": 2,
-        "desc_start": "A blast of rainbow energy",
-        "desc_inc": "blast of rainbow energy",
-        "cast_action": "chant"
-    },
+    # Chaos magic.
+    "chaosbolt": {},
     "entropy field": {},
+    "wild weapon": {},
     "chaotic bombardment": {},
-    "firebolt": {
-        "spellfunction": spell_attack_status,
-        "cost": 2,
-        "target": "other",
-        "elemental": "fire",
-        "status": "burning",
-        "status_chance": 25,
-        "status_duration": 2,
-        "accuracy": 85,
-        "min_dmg": 2,
-        "desc_start": "A blast of flames",
-        "desc_inc": "blast of flames",
-        "cast_action": "chant"
-    },
-    "will-o-wisp": {
-        "spellfuction": spell_protect,
-        "cost": 5,
-        "target": "anychar",
-        "elemental": "fire",
-        "accuracy": 100,
-        "max_hp": 15,
-        "min_dmg": 0,
-        "status": "protect",
-        "status_duration": 999,
-        "status_chance": 100,
-        "desc_start": "A ruby colored bubble",
-        "desc_inc": "a ruby colored bubble",
-        "cast_action": "signs"
-    },
-    "phoenix weapon": {
-        "spellfunction": spell_buff_noprotect,
-        "cost": 3,
-        "target": "anyobj",
-        "elemental": "fire",
-        "accuracy": 100,
-        "min_dmg": 0,
-        "status": "blazing",
-        "status_chance": 100,
-        "status_duration": 5,
-        "noncombat_spell": False,
-        "desc_start": "Igniting in flames",
-        "desc_inc": "ignites in flames",
-        "cast_action": "scribe"
-    },
-    "phoenix flare": {
-        "spellfunction": spell_attack_status,
-        "cost": 10,
-        "target": "eparty",
-        "elemental": "fire",
-        "min_dmg": 7,
-        "status": "burning",
-        "status_chance": 100,
-        "status_duration": 5,
-        "noncombat_spell": False,
-        "desc_start": "A wave of white hot flames",
-        "desc_in": "waves of white hot flames",
-        "cast_action": "chant",
-        "dice_level": ['10','10','10','12','12','12','20','20','20']
-    },
-    "salamander": {
-        "spellfunction": spell_summon,
-        "cost": 20,
-        "target": "none",
-        "min_dmg": 0,
-        "object_key": "Salamander",
-        "accuracy": 95,
-        "pvp_spell": False,
-        "desc_start": "Opens a fiery portal summoning Salamander",
-        "desc_inc": "summons Salamander from within a fiery portal",
-        "cast_action": "chant",
-        "max_rank": 1
-    }
+    # Time Magic
+    "haste": {},
+    "slow": {},
+    "stop": {},
+    "gravity crush": {},
+    # healing magic
+    "healing energy": {},
+    "liminal energy": {},
+    "regeneration": {},
+    # abjuration magic
+    "protect": {},
+    "reflection": {},
+    # mana magic
+    "manabolt": {},
+    "counterspell": {},
+    "arcane strike": {},
+    "mana weapon": {},
+    # ice magic
+    "arcticbolt": {},
+    "snowstorm": {},
+    "blizzard weapon": {},
+    "arctic snap": {},
+    # holy magic (light)
+    "lightbolt": {},
+    "gleaming weapon": {},
+    "gleaming aura": {},
+    "divine judgment": {},
+    "sanctuary": {},
+    "rebuke": {},
+    # shadow magic
+    "voidbolt": {},
+    "blackhole horizon": {},
+    "shadow weapon": {},
+    "blackhole supernova": {},
+    # earth magic
+    "geobolt": {},
+    "sandstorm weapon": {},
+    "seismic tremor": {},
+    "earthquake": {},
+    # air magic
+    "aerobolt": {},
+    "tornado weapon": {},
+    "windscreen": {},
+    "heaven's shockwave": {},
+    # water magic
+    "aquabolt": {},
+    "bubble screen": {},
+    "whirlpool weapon": {},
+    "tidal shockwave": {},
+    # Fire magic
+    "firebolt": {},
+    "will-o-wisp": {},
+    "phoenix weapon": {},
+    "phoenix flare": {}
 }
