390c039c95b03b659be75a08eea8dd815de8add5
D "Seamus" Mitchell <g33kcub@gmail.com>
2019-12-29 19:28:42 -0500

    minor d2 thoughts.

--- world/kumarpg/dicts/d2_title_def.py
+++ world/kumarpg/dicts/d2_title_def.py
@@ -4,7 +4,9 @@ D2_TITLES = {
     "ninja": types["title_dict"],
     "samurai": types["title_dict"],
     "onmyoji": types["title_dict"],
+    "geisha": types["title_dict"],
     "deep wizard": types["title_dict"], #attaining one of the deep magics. 
     "the deep wizard": types["title_dict"], #the first to attain all deep magics. 
-    "arthen champion": types["title_dict"] #killing every floor boss at least once. 
+    "tykeno champion": types["title_dict"], #killing every floor boss at least once. 
+    "dungeon master": types["title_dict"] #Defeating every final boss in every dungeon.
     }
