4b9b3f7d72eadaf90bfe8f8532d09d60377c3fc8
D "Seamus" Mitchell <g33kcub@gmail.com>
2020-01-09 21:01:42 -0500

    Update d1_stat_defs.py

--- world/kumarpg/dicts/d1_stat_defs.py
+++ world/kumarpg/dicts/d1_stat_defs.py
@@ -7,3 +7,13 @@ stats = {
     "water": types["stat_dict"],
     "void": types["stat_dict"]
 }
+
+earth_techniques = {
+    "bulletproof": types["tech_dict"]
+}
+
+void_techniques = {
+    "mana rush": types["tech_dict"],
+    "spirituality": types["tech_dict"],
+    "blood magic": types["tech_dict"]
+}
